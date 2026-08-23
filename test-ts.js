e3ad674f74a123bd8eea47dd0ac2440f99408161
Achukatlu Kaif <kaifachukatlu@gmail.com>
2026-08-23 21:25:07 +0530

    chore: cleanup test scripts

--- test-ts.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config({ path: '.env' });
-const { sendEmail } = require('./src/lib/email.ts'); // Wait, require can't load TS directly without ts-node
-
-console.log("Testing email directly...");
